5e4d0f042823c7a38fd4c06b6c2dcac9c61a1cfb
Stefan Weiberg <stefan.weiberg@gmail.com>
2014-05-21 11:15:40 +0200

    ...

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,1 +1,42 @@
-module Parser where
+module Parser ( parse {- nur parse exportieren -} )
+
+    where
+
+import           IR
+import           Scanner
+
+-- Der Parser versucht aus einer Liste von MDToken einen AST zu erzeugen 
+parse :: [MDToken] -> Maybe AST
+-- Die leere Liste ergibt eine leere Sequenz
+parse []                       = Just $ Sequence []
+-- Zwei Zeilenumbrüche hintereinander sind eine leere Zeile, die in eine Sequenz eingeführt wird (wirklich immer?)
+parse (T_Newline:T_Newline:xs) = maybe Nothing (\(Sequence ast) -> Just $ Sequence (EmptyLine : ast)) $ parse xs
+-- ein einzelnes Leerzeichen ignorieren wir (für den Moment?)
+parse (T_Newline:xs)           = parse xs
+-- einem Header muss ein Text folgen. Das ergibt zusammen einen Header im AST, er wird einer Sequenz hinzugefügt
+parse (T_H i : T_Text str: xs) = maybe Nothing (\(Sequence ast) -> Just $ Sequence (H i str:ast)) $ parse xs
+-- einem listitem-Marker muss auch ein Text folgen. Das gibt zusammen ein Listitem im AST.
+-- es wird mit der Hilfsfunktion addLI eingefügt
+parse (T_ULI i: T_Text str: xs) = maybe Nothing (\ast -> Just $ addULI (LI str) ast) $ parse xs
+-- ein Text am Anfang gehört in einen Absatz. Damit direkt auf einander folgende Texte in einem gemeinsamen
+-- Absatz landen, wird die Hilfsfunktion addP genutzt um den Text einzufügen
+parse (T_Text str: xs)         = maybe Nothing (\ast -> Just $ addP (P str) ast) $ parse xs
+-- Der gesamte Rest wird für den Moment ignoriert. Achtung: Der Parser schlägt, in der momentanen Implementierung, nie fehl.
+-- Das kann in der Endfassung natürlich nicht so bleiben!
+parse _ = Just $ Sequence []
+
+-- Hilfsfunktionen für den Parser
+
+-- Einfügen eines Listenelements in eine ungeordnete Liste
+addULI :: AST -> AST -> AST
+-- Wenn wir ein Listenelement einfügen wollen und im Rest schon eine UL haben, fügen wir das Element in die UL ein
+addULI li (Sequence (UL lis : ast)) = Sequence (UL (li:lis) : ast)
+-- Andernfalls erzeugen wir eine neue UL.
+addULI li (Sequence ast) = Sequence (UL [li] : ast)
+
+-- Mehrere aufeinander folgende Texte werden zu einem Absatz zusammengefügt.
+addP :: AST -> AST -> AST
+-- Wenn wir zwei Absätze hintereinander finden, fassen wir diese zusammen 
+addP (P str1) (Sequence (P str2 : ast)) = Sequence (P (str1 ++ "\n" ++ str2) : ast)
+-- Andernfalls bleibt der Absatz alleine
+addP p (Sequence ast) = Sequence (p : ast)
---
+++ src/Scanner.hs
@@ -0,0 +1,28 @@
+module Scanner where
+
+-- MD: Markdown
+data MDToken = T_Newline     -- '\n' 
+             | T_H Int       -- ein Header mit der Anzahl der Hashes
+             | T_Text String -- Text, aber immer nur bis zum Zeilenende, Text über mehrere Zeilen muss vom Parser zusammengesetzt werden
+             | T_ULI Int     -- ein ungeordnetes Listenelement-Marker mit der (Einrückungs-)Ebene
+    deriving (Show, Eq)
+
+scan :: String -> Maybe [MDToken]
+-- Rekursionsende
+scan ""           = Just []
+-- eine Überschrift
+scan str@('#':xs) =
+        -- String aufteilen in Hashes und Rest
+    let (hashes, rest) = span (=='#') str
+        -- Anzahl der Hashes ergibt das Level, aber höchstens 6 werden gezählt, der Rest ignoriert
+        level = min (length hashes) 6
+    in maybe Nothing (\tokens -> Just (T_H level:tokens))      $ scan rest
+-- Zeilenumbrüche aufheben um im Parser Leerzeilen zu erkennen
+scan ('\n':xs)    = maybe Nothing (\tokens -> Just (T_Newline:tokens)) $ scan xs
+-- wenn das '-' am Zeilenanfang gelesen wird, ist es Level 0
+-- TODO: noch sind wir sicher am Zeilenanfang, aber nicht mehr unbedingt, wenn wir weitere Fälle einbauen (Links etc.)
+scan ('-':xs)     = maybe Nothing (\tokens -> Just (T_ULI 0:tokens))    $ scan xs
+-- sonst lesen wir einfach den Rest bis zum Zeilenende in ein Text-Token ein
+scan str          =
+    let (restOfLine, restOfStr) = span (/='\n') str
+    in maybe Nothing (\tokens -> Just (T_Text restOfLine:tokens)) $ scan restOfStr
