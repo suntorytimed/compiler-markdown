87d4a64e663a1b11a4f365cfef8c4849cf974735
Jennifer Liebel <liebel@hm.edu>
2014-06-05 10:21:45 +0000

    Whitespaces eingefuegt und Pruefung auf Zahl

--- src/Scanner.hs
+++ src/Scanner.hs
@@ -7,26 +7,27 @@ data MDToken = T_Newline     -- '\n'
              | T_ULI Int     -- ein ungeordnetes Listenelement-Marker mit der (Einrückungs-)Ebene
              | T_SLI Int     -- ein geordnetes Listenelement-Marker mit der (Einrückungs-)Ebene
              | T_White Int   -- ein Header mit der Anzahl der Hashes
+             | T_Num         -- eine Zahl
     deriving (Show, Eq)
 
 scan :: String -> Maybe [MDToken]
 -- Rekursionsende
 scan ""           = Just []
 -- eine Überschrift
-scan str@('#':xs) =
-        -- String aufteilen in Hashes und Rest
-    let (hashes, rest) = span (=='#') str
-        -- Anzahl der Hashes ergibt das Level, aber höchstens 6 werden gezählt, der Rest ignoriert
-        level = min (length hashes) 6
+scan str@(x:xs)
+    -- String aufteilen in Hashes und Rest
+    |x == '#' = let (hashes, rest) = span (=='#') str
+                -- Anzahl der Hashes ergibt das Level, aber höchstens 6 werden gezählt, der Rest ignoriert
+                    level = min (length hashes) 6
     in maybe Nothing (\tokens -> Just (T_H level:tokens))      $ scan rest
-    
-    -- bei 2 oder mehr Leerzeichen wird eine neue Zeile eingefuegt
-scan str@(' ':xs) =
-        -- String aufteilen in Whitespaces und Rest
-    let (whitespace, rest) = span (==' ') str
-        -- Anzahl der Whitespaces ist egal, es wird immer nur eine Newline eingefuegt
-        level = (length whitespace)
+
+    -- String aufteilen in Whitespaces und Rest
+    |x == ' ' = let (whitespace, rest) = span (==' ') str
+                    -- Anzahl der Whitespaces ist egal, es wird immer nur eine Newline eingefuegt
+                    level = (length whitespace)
     in maybe Nothing (\tokens -> Just (T_White level:tokens))     $ scan rest
+
+    |isDigit x = maybe Nothing (\tokens -> Just (T_Num:tokens))
 
 -- sonst lesen wir einfach den Rest bis zum Zeilenende in ein Text-Token ein
 -- Zeilenumbrüche aufheben um im Parser Leerzeilen zu erkennen
@@ -39,7 +40,7 @@ scan ('+':xs)     = maybe Nothing (\tokens -> Just (T_ULI 0:tokens))    $ scan x
 scan ('*':xs)     = maybe Nothing (\tokens -> Just (T_ULI 0:tokens))    $ scan xs
 
 -- wenn eine Zahl gefolgt von einem Punkt da steht, dann soll eine geordnete Liste erzeugt werden
---scan ('???':xs)     = maybe Nothing (\tokens -> Just (T_SLI 0:tokens))    $ scan xs
+--scan (:xs)     = maybe Nothing (\tokens -> Just (T_SLI 0:tokens))    $ scan xs
 
 scan str          =
     let (restOfLine, restOfStr) = span (/='\n') str
